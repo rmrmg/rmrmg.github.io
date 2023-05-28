b230966bb434cf174adf64a5c0c1d74dbe8f68ee
Rafał Roszak <rmrmg@wp.pl>
2023-05-28 21:41:48 +0200

    second

--- js/src.js
+++ js/src.js
@@ -1,20 +1,28 @@
 //code here
-function generateTable() {
+function generateTable(rzedy, kolumny) {
+  if (document.getElementById('tabela1')) {
+    alert('juz jest')
+    return;
+  }
+  const rzad  = document.getElementById(rzedy).value;
+  const kolumn = document.getElementById(kolumny).value;
   // creates a <table> element and a <tbody> element
+  const div = document.createElement("div");
   const tbl = document.createElement("table");
+  div.setAttribute("id", "tabela1");
   const tblBody = document.createElement("tbody");
 
   // creating all cells
-  for (let i = 0; i < 2; i++) {
+  for (let i = 1; i <= rzad; i++) {
     // creates a table row
     const row = document.createElement("tr");
 
-    for (let j = 0; j < 2; j++) {
+    for (let j = 1; j <= kolumn; j++) {
       // Create a <td> element and a text node, make the text
       // node the contents of the <td>, and put the <td> at
       // the end of the table row
       const cell = document.createElement("td");
-      const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+      const cellText = document.createTextNode(` ${i} ${j}`);
       cell.appendChild(cellText);
       row.appendChild(cell);
     }
@@ -25,8 +33,13 @@ function generateTable() {
 
   // put the <tbody> in the <table>
   tbl.appendChild(tblBody);
+  div.appendChild(tbl)
+  //
+  const przycisk = document.createElement('button')
+  przycisk.innerHTML='skasuj'
+  div.appendChild(przycisk)
   // appends <table> into <body>
-  document.body.appendChild(tbl);
+  document.body.appendChild(div);
   // sets the border attribute of tbl to '2'
   tbl.setAttribute("border", "2");
 }
